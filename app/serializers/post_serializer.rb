5a3be10131f8db3f31d95cafec5dd0f626eb2912
alik864 <alikarukas@gmail.com>
2015-06-05 16:55:04 -0400

    updatewith css and works

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :date
+
+  def date
+    object.created_at.strftime("%m/%d/%Y")
+  end
 end
---
+++ db/migrate/20150605192036_createcategorypost.rb
@@ -0,0 +1,4 @@
+class Createcategorypost < ActiveRecord::Migration
+  def change
+  end
+end
